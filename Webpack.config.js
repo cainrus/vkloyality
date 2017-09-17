b5768dc603d1aacc6106a4b5a64eaa863299ab91
bessgeor <bessgeor@gmail.com>
2017-09-18 02:11:00 +0300

    Webpack config for gluing it all together. Now build command is 'npm i && "./node_modules/.bin/webpack" && npm run build'

---
+++ Webpack.config.js
@@ -0,0 +1,35 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: "./src/popup/cardchecker.tsx",
+  output: {
+    filename: "popup.js",
+    path: path.resolve(__dirname, "extension/build")
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: '__MSG_appName__',
+      filename: path.resolve(__dirname, 'extension/build/popup.html')
+    })
+  ],
+
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: "source-map",
+
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".ts", ".tsx", ".js", ".json", ".ejs"]
+  },
+
+  module: {
+    rules: [
+      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+    ]
+  }
+};
