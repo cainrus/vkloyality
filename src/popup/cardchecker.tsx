a03a0bdffe9a2406243d0620891e7d975c087d2f
bessgeor <bessgeor@gmail.com>
2017-09-25 23:29:34 +0300

    Browser popup init

---
+++ src/popup/cardchecker.tsx
@@ -0,0 +1,11 @@
+﻿import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+class CardChecker extends React.Component<{}, {}>
+{
+  render() {
+    return <p>I'm here</p>
+  }
+}
+
+ReactDOM.render(<CardChecker />, document.getElementsByTagName( 'div' )[ 0 ]);
