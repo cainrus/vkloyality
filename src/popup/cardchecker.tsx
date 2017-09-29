976b97ac30471bd9306fa74e894facb76a6d5695
bessgeor <bessgeor@gmail.com>
2017-09-29 20:41:31 +0300

    Added error display

--- src/popup/cardchecker.tsx
+++ src/popup/cardchecker.tsx
@@ -11,16 +11,19 @@ interface ICardCheckerState
   IsChecking: boolean;
   IsSwindler: string;
   CardNumber: string;
+  error?: any;
 }
 
 class CardChecker extends React.Component<{}, ICardCheckerState>
 {
   state: ICardCheckerState; // a hack to allow state mutability which is prohibited by react typings
 
+  getDefaultState: () => ICardCheckerState = () => ({ CardNumber: "", IsChecking: false, IsSwindler: null });
+
   constructor()
   {
     super();
-    this.state = { CardNumber: "", IsChecking: false, IsSwindler: null };
+    this.state = this.getDefaultState();
   }
 
   updateState: (e: React.ChangeEvent<HTMLInputElement>) => boolean = e =>
@@ -39,14 +42,24 @@ class CardChecker extends React.Component<{}, ICardCheckerState>
     return true;
   }
 
+  resetState: () => void = () =>
+  {
+    this.state = this.getDefaultState();
+    this.setState(this.state);
+  }
+
   render() {
-    return <div>
+    return this.state.error
+      ? <div>
+          <p className="error">{this.state.error.message} happens. Please contact the <a href="https://vk.com/b.geor">author</a></p>
+          <button onClick={this.resetState}>Try Again</button>
+        </div>
+      : <div>
         <input onChange={this.updateState} placeholder="enter payment props to check here" value={this.state.CardNumber} readOnly={this.state.IsChecking} />
         {this.state.IsSwindler != null
           ? <div>
             <h3>Warning: swindler found</h3>
             <a href={this.state.IsSwindler} target="_blank">Learn more...</a>
-            <button onClick={this.resetState}>Check another</button>
           </div>
           : this.state.IsChecking
             ? <p> throbber here</p>
